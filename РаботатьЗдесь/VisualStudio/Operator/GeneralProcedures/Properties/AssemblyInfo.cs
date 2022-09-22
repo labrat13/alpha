7b00f754fc7d37a28d193e1202d6063e20192044
John Smith <jsmith@mail.com>
2022-09-22 18:34:42 +0700

    Create new Operator v1.1
    Replace existing project source with new source  for Operator v1.1 console version:
    - Previous source v1.0 moved to Archive folder/
    - Create new solution VS2022
    - Add initial stuff files to solution.
    - Target: create new version of Operator with improved Procedure subsystem.

---
+++ РаботатьЗдесь/VisualStudio/Operator/GeneralProcedures/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("GeneralProcedures")]
+[assembly: AssemblyDescription("Virtual assistance general procedures library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Seliakov Pavel")]
+[assembly: AssemblyProduct("Operator")]
+[assembly: AssemblyCopyright("Copyright ©  2022 Seliakov Pavel")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("94e8f9e4-d1c6-4551-9cfd-ebbd0444519f")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
---
+++ РаботатьЗдесь/VisualStudio/Operator/Operator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Operator")]
+[assembly: AssemblyDescription("Virtual assistance console application")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Seliakov Pavel")]
+[assembly: AssemblyProduct("Operator")]
+[assembly: AssemblyCopyright("Copyright ©  2022 Seliakov Pavel")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("9981607a-af72-4aa7-bcb1-5c47cc6c2c6e")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
