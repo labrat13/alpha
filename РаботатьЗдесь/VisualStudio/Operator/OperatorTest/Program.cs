dd48672fcbc08addbd42d663bb876c102a6750e9
John Smith <jsmith@mail.com>
2022-10-06 12:46:19 +0700

    port up

---
+++ РаботатьЗдесь/VisualStudio/Operator/OperatorTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Engine.DbSubsystem;
+
+namespace OperatorTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create database
+            OperatorDbAdapter db = new OperatorDbAdapter(null);
+            String filepath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            filepath = Path.Combine(filepath, OperatorDbAdapter.AppDbFileName);
+            string connString = OperatorDbAdapter.CreateConnectionString(filepath, false);
+            if (!File.Exists(filepath))
+            {
+                OperatorDbAdapter.DatabaseCreate(filepath);
+                db.Open(connString);
+                //
+                db.CreateDatabaseTables();
+                //
+                db.Close();
+            }
+            //create 3 ot more table items and add, update and delete them.
+
+
+        }
+    }
+}
